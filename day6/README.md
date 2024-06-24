20f76484c6b4edee6cb2a7ec5b0bbd3fe5708d5e
Antonio Ramirez <aramirezmartin@hotmail.com>
2024-06-24 23:35:20 +0200

    1st part day 6

--- day6/README.md
+++ day6/README.md
@@ -13,3 +13,5 @@ turn on 0,0 through 999,999 would turn on (or leave on) every light.
 toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
 turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
 After following the instructions, how many lights are lit?
+
+Your puzzle answer was 543903.
--- day6/day6_a.c
+++ day6/day6_a.c
@@ -1,5 +1,7 @@
 #include "../inc/advent.h"
 
+int	matrix_size = 1000; 
+
 typedef struct s_coordinate
 {
 	int	x;
@@ -14,41 +16,111 @@ typedef struct s_datos
 
 void	extract_data(char *str, t_data *data)
 {
-	
+	while (isdigit(*str) == 0)
+		str++;
+	data->start.x = atoi(str);
+	while (*str != ',')
+		str++;
+	str++;
+	data->start.y = atoi(str);
+	while (isdigit(*str) != 0)
+		str++;
+	while (isdigit(*str) == 0)
+		str++;
+	data->end.x = atoi(str);
+	while (*str != ',')
+		str++;
+	str++;
+	data->end.y = atoi(str);
 }
 
-static void	day6(char *str, char **matrix)
+static void	toggle(int **matrix, t_data *data)
+{
+	if (data->start.x < 0 || data->end.x >= matrix_size || data->start.y < 0 || data->end.y >= matrix_size)
+	{
+		fprintf(stderr, "Error: Indices fuera de los límites de la matriz.\n");
+		return;
+	}
+
+	for(int x = data->start.x; x <= data->end.x; x++)
+	{
+		for(int y = data->start.y; y <= data->end.y; y++)
+		{
+			if (matrix[x][y] == 0)
+				matrix[x][y] = 1;
+			else if (matrix[x][y] == 1)
+				matrix[x][y] = 0;
+			else
+			{
+				fprintf(stderr, "Error: Valor inesperado en matrix[%d][%d] = %d\n", x, y, matrix[x][y]);
+				return;
+			}
+		}
+	}
+}
+
+static void	turn_on(int **matrix, t_data *data)
+{
+	for(int x = data->start.x; x <= data->end.x; x++)
+		for(int y = data->start.y; y <= data->end.y; y++)
+			matrix[x][y] = 1;
+}
+
+static void	turn_off(int **matrix, t_data *data)
+{
+	for(int x = data->start.x; x <= data->end.x; x++)
+		for(int y = data->start.y; y <= data->end.y; y++)
+			matrix[x][y] = 0;
+}
+
+static int	count_lights(int **matrix)
+{
+	int	lights;
+
+	lights = 0;
+	for(int x = 0; x < matrix_size; x++)
+		for(int y = 0; y < matrix_size; y++)
+			if (matrix[x][y] == 1)
+				lights++;
+	return (lights);
+}
+
+static int	day6(char *str, int **matrix)
 {
 	t_data	data;
+	int		lights;
 
-	extract_data(str, &data);
-	/*
+	extract_data(str, &data);	
 	if (str[1] == 'o')
 	{
-		toggle();
+		toggle(matrix, &data);
 	}
-	if (str[6] == 'f')
+	if (str[6] == 'f')  
 	{
-		turn_off("off ");
+		turn_off(matrix, &data);
 	}
 	if (str[6] == 'n')
 	{
-		turn_on
+		turn_on(matrix, &data);
 	}
-		printf("on ");
-		*/
+	lights = count_lights(matrix);
+	
+	return (lights);
 }
-/*
-Hacer las tres una función?
-static void	toggle(char **matrix, t_datos datos)
-static void	turn_on(char **matrix, t_datos datos)
-static void	turn_off(char **matrix, t_datos datos)
-*/
+
+static void init(int **matrix)
+{
+	for(int x = 0; x < matrix_size; x++)
+		for(int y = 0; y < matrix_size; y++)
+			matrix[x][y] = 0;
+}
+
 int main(int argc, char **argv)
 {
 	FILE	*file;
 	char	buffer[MAX_LINE_LENGTH];
-	char	**matrix;
+	int		**matrix;
+	int		lights;
 
 	(void)argc;
 	if (!argv[1])
@@ -62,10 +134,12 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error opening text file");
 		exit(1);
 	}
-	matrix = create_matrix(1000);
+	matrix = create_matrix(matrix_size);
+	init(matrix);
 	while (fgets(buffer, MAX_LINE_LENGTH, file) != 0)
-		day6(buffer, matrix);
+		lights = day6(buffer, matrix);
+	printf("Total lights on = %d", lights);
 	fclose(file);
-	free_matrix(matrix, 1000);
+	free_matrix(matrix, matrix_size);
 	return (0);
 }
